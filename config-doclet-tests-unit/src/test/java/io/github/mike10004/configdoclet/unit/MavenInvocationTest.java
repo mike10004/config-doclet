8bd3e005d9356918ce2bcd698914d8c9c49400f8
Mike Chaberski <mchaberski@gmail.com>
2018-08-08 17:04:22 -0400

    detect maven home directory

--- config-doclet-tests-unit/src/test/java/io/github/mike10004/configdoclet/unit/MavenInvocationTest.java
+++ config-doclet-tests-unit/src/test/java/io/github/mike10004/configdoclet/unit/MavenInvocationTest.java
@@ -6,15 +6,22 @@ import org.apache.maven.shared.invoker.DefaultInvoker;
 import org.apache.maven.shared.invoker.InvocationRequest;
 import org.apache.maven.shared.invoker.InvocationResult;
 import org.apache.maven.shared.utils.cli.CommandLineException;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Objects;
 import java.util.Properties;
+import java.util.function.Supplier;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.Assert.assertEquals;
@@ -26,6 +33,13 @@ public class MavenInvocationTest {
     @Rule
     public final TemporaryFolder temporaryFolder = new TemporaryFolder();
 
+    private static File mavenHomeDir;
+
+    @BeforeClass
+    public static void detectMavenHome() throws IOException {
+        mavenHomeDir = resolveMavenHome();
+    }
+
     @Test
     public void invoke() throws Exception {
         Path projectDir = temporaryFolder.newFolder().toPath();
@@ -35,8 +49,6 @@ public class MavenInvocationTest {
         request.setPomFile(projectDir.resolve("pom.xml").toFile());
         request.setGoals(Collections.singletonList("javadoc:javadoc"));
         Properties properties = new Properties();
-//        String docletClasspath = resolveDocletJars().stream().map(File::getAbsolutePath).collect(Collectors.joining(File.pathSeparator));
-//        properties.setProperty("docletPath", docletClasspath);
         properties.setProperty("java.home", System.getProperty("java.home"));
         request.setProperties(properties);
         System.out.format("passing JAVA_HOME=%s%n", System.getProperty("java.home"));
@@ -46,7 +58,7 @@ public class MavenInvocationTest {
         request.addShellEnvironment("DOCLET_ARTIFACT_ARTIFACT_ID", "config-doclet-core");
         request.addShellEnvironment("DOCLET_ARTIFACT_VERSION", tc.get("project.version"));
         DefaultInvoker invoker = new DefaultInvoker();
-        invoker.setMavenHome(resolveMavenHome());
+        invoker.setMavenHome(mavenHomeDir);
         invoker.setWorkingDirectory(temporaryFolder.newFolder());
         InvocationResult result = invoker.execute(request);
         CommandLineException exception = result.getExecutionException();
@@ -61,25 +73,44 @@ public class MavenInvocationTest {
         assertFalse("content nonempty", content.trim().isEmpty());
     }
 
-    private File resolveMavenHome() {
-        // TODO allow specifying maven home with sysprop
-        return new File("/usr/share/maven");
+    private static File resolveMavenHome() throws IOException {
+        for (Supplier<String> supplier : getMavenHomePathnameSuppliers()) {
+            String dir = supplier.get();
+            if (dir != null) {
+                File dirPathname = new File(dir);
+                if (dirPathname.isDirectory()) {
+                    File mvnExecutable = new File(dirPathname, "bin/mvn");
+                    if (!mvnExecutable.isFile()) {
+                        System.err.format("resolved maven home at %s but executable %s does not exist%n", dirPathname.getAbsolutePath(), mvnExecutable.getAbsolutePath());
+                    }
+                }
+                return dirPathname;
+            }
+        }
+        throw new IOException("maven home directory not found");
     }
 
-//    private List<File> resolveDocletJars() throws IOException {
-//        List<File> files = new ArrayList<>();
-//        Resources.asCharSource(getClass().getResource("/dependencies.txt"), UTF_8).forEachLine(line -> {
-//            if (line.startsWith("  ")) {
-//                String untokenized = Splitter.on(" -- ").split(line).iterator().next();
-//                Iterator<String> tokenized = Splitter.on(":").split(untokenized).iterator();
-//                String path = null;
-//                while (tokenized.hasNext()) {
-//                    path = tokenized.next();
-//                }
-//                checkState(path != null);
-//                files.add(new File(path));
-//            }
-//        });
-//        return files;
-//    }
+    private static Iterable<Supplier<String>> getMavenHomePathnameSuppliers() {
+        return java.util.List.of(
+                () -> System.getProperty("configdoclet.build.maven.home"),
+                () -> System.getenv("M2_HOME"),
+                () -> {
+                    String[] pathDirs = System.getenv("PATH").split(Pattern.quote(File.pathSeparator));
+                    return Arrays.stream(pathDirs)
+                            .flatMap(dir -> {
+                                return Stream.of("mvn", "mvnw.bat").map(binname -> new File(dir, binname));
+                            }).filter(File::isFile)
+                            .map(File::getParentFile) // to Maven home/bin dir
+                            .filter(Objects::nonNull)
+                            .map(File::getParentFile) // to Maven home dir
+                            .filter(Objects::nonNull)
+                            .map(File::getAbsolutePath)
+                            .findFirst().orElse(null);
+                },
+                () -> "/usr/local/share/maven",
+                () -> "/usr/share/maven"
+
+        );
+    }
+
 }
