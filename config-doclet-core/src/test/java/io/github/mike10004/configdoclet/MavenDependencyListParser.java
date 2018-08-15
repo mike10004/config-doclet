2209d861165600367d14a2e7cdccdcda6e21a350
Mike Chaberski <mchaberski@gmail.com>
2018-08-15 13:22:29 -0400

    support running tests in console with colors on
    Bash control codes that change text color were affecting the way the dependency list used by ConfigDocletTest was parsed, corrupting the classpath.

--- config-doclet-core/src/test/java/io/github/mike10004/configdoclet/MavenDependencyListParser.java
+++ config-doclet-core/src/test/java/io/github/mike10004/configdoclet/MavenDependencyListParser.java
@@ -1,6 +1,8 @@
 package io.github.mike10004.configdoclet;
 
+import com.google.common.base.CharMatcher;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.text.CharacterPredicate;
 
 import javax.annotation.Nullable;
 import java.io.BufferedReader;
@@ -13,6 +15,8 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static com.google.common.base.Preconditions.checkState;
 
@@ -35,11 +39,41 @@ public class MavenDependencyListParser {
     private static final String TOKEN_SPLITTER_REGEX = ":";
     private static final int MAX_ARTIFACT_TOKENS = 6;
 
+    static String trimTrailingWhitespace(String str) {
+        return trimTrailing(str, Character::isWhitespace);
+    }
+
+    interface PrimitiveCharPredicate {
+        boolean test(char ch);
+
+        default PrimitiveCharPredicate or(PrimitiveCharPredicate other) {
+            return ch -> PrimitiveCharPredicate.this.test(ch) || other.test(ch);
+        }
+    }
+
+    static String retainWhile(String s, PrimitiveCharPredicate predicate) {
+        for (int i = 0; i < s.length(); i++) {
+            if (!predicate.test(s.charAt(i))) {
+                return s.substring(0, i);
+            }
+        }
+        return s;
+    }
+
+    static String trimTrailing(String str, PrimitiveCharPredicate predicate) {
+        int stop = str.length();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            char ch = str.charAt(i);
+            if (!predicate.test(ch)) {
+                break;
+            }
+            stop = i;
+        }
+        return str.substring(0, stop);
+    }
+
     protected MavenRepositoryItem parseLine(String line) {
         line = line.split("\\s*--\\s*", 2)[0];
-        if (line.matches(".*\\Q (optional)\\E")) {
-            line = StringUtils.removeEnd(line, " (optional)");
-        }
         Iterator<String> tokens = Arrays.asList(line.split(TOKEN_SPLITTER_REGEX, MAX_ARTIFACT_TOKENS)).iterator();
         // e.g. "org.hamcrest:hamcrest-core:jar:1.3:test"
         checkState(tokens.hasNext(), "empty: %s", line);
@@ -55,12 +89,31 @@ public class MavenDependencyListParser {
         @Nullable String rawArtifactPathname = null;
         if (tokens.hasNext()) {
             rawArtifactPathname = tokens.next();
+            rawArtifactPathname = cleanRawPathname(rawArtifactPathname);
         }
         Optional<File> f = Optional.ofNullable(rawArtifactPathname).map(File::new);
         MavenCoordinates coords = new MavenCoordinates(groupId, artifactId, version, null);
         MavenDependency dep = new MavenDependency(coords, type, scope);
         return new MavenRepositoryItem(dep, f.orElse(null), rawArtifactPathname);
     }
+
+    static String cleanRawPathname(String s) {
+        if (s.contains("(optional)")) {
+            Matcher m = Pattern.compile("^(.*)\\(optional\\).*$").matcher(s);
+            if (m.find()) {
+                s = m.group(1);
+            }
+        }
+        s = retainWhile(s, IS_ASCII_NON_CONTROL);
+        s = trimTrailingWhitespace(s);
+        return s;
+    }
+
+    private static final CharMatcher ASCII = CharMatcher.ascii();
+
+    private static final PrimitiveCharPredicate IS_ASCII_NON_CONTROL = ch -> {
+        return ASCII.matches(ch) && !Character.isISOControl(ch);
+    };
 
     private class ListLineParser {
 
--- config-doclet-core/src/test/java/io/github/mike10004/configdoclet/MavenDependencyListParserTest.java
+++ config-doclet-core/src/test/java/io/github/mike10004/configdoclet/MavenDependencyListParserTest.java
@@ -17,6 +17,7 @@ public class MavenDependencyListParserTest {
         String text = "\n" +
                 "The following files have been resolved:\n" +
                 "   com.google.code.findbugs:jsr305:jar:3.0.2:compile:/home/mike/.m2/repository/com/google/code/findbugs/jsr305/3.0.2/jsr305-3.0.2.jar (optional)  -- module jsr305 (auto)\n" +
+                "   com.example:with-spaces-in-path:jar:2.0:compile:/home/John Smith/.m2/repository/com/example/with-spaces-in-path/with-spaces-in-path-2.0.jar (optional)  -- module gson (auto)\n" +
                 "   com.google.code.gson:gson:jar:2.8.5:compile:/home/mike/.m2/repository/com/google/code/gson/gson/2.8.5/gson-2.8.5.jar (optional)  -- module gson (auto)\n" +
                 "\n";
 
@@ -24,6 +25,27 @@ public class MavenDependencyListParserTest {
         MavenRepositoryItem item0 = items.get(0);
         assertNotNull(item0.artifactPathname);
         assertEquals(new File("/home/mike/.m2/repository/com/google/code/findbugs/jsr305/3.0.2/jsr305-3.0.2.jar"), item0.artifactPathname);
+        MavenRepositoryItem withSpaces = items.get(1);
+        assertEquals("rawPathname", "/home/John Smith/.m2/repository/com/example/with-spaces-in-path/with-spaces-in-path-2.0.jar", withSpaces.rawPathname);
+    }
+
+    @Test
+    public void parseAll_colors() throws Exception {
+        String text = "\n" +
+                "The following files have been resolved:\n" +
+                "   com.google.code.findbugs:jsr305:jar:3.0.2:compile:/home/mike/.m2/repository/com/google/code/findbugs/jsr305/3.0.2/jsr305-3.0.2.jar (optional) \u001B[36m -- module jsr305\u001B[0;1;33m (auto)\u001B[m\n" +
+                "   org.apache.commons:commons-lang3:jar:3.6:compile:/home/mike/.m2/repository/org/apache/commons/commons-lang3/3.6/commons-lang3-3.6.jar\u001B[36m -- module org.apache.commons.lang3\u001B[0;1m [auto]\u001B[m\n" +
+                "   com.google.code.gson:gson:jar:2.8.5:compile:/home/mike/.m2/repository/com/google/code/gson/gson/2.8.5/gson-2.8.5.jar (optional) \u001B[36m -- module gson\u001B[0;1;33m (auto)\u001B[m\n" +
+                "\n";
+
+        List<MavenRepositoryItem> items = new MavenDependencyListParser().parseList(new StringReader(text));
+        MavenRepositoryItem jsr = items.get(0);
+        assertEquals("rawPathname jsr", "/home/mike/.m2/repository/com/google/code/findbugs/jsr305/3.0.2/jsr305-3.0.2.jar", jsr.rawPathname);
+        MavenRepositoryItem lang3 = items.get(1);
+        assertEquals("artifactId", "commons-lang3", lang3.dependency.artifact.getArtifactId());
+        assertEquals("rawPathname", "/home/mike/.m2/repository/org/apache/commons/commons-lang3/3.6/commons-lang3-3.6.jar", lang3.rawPathname);
+        MavenRepositoryItem gson = items.get(2);
+        assertEquals("gson rawPathname", "/home/mike/.m2/repository/com/google/code/gson/gson/2.8.5/gson-2.8.5.jar", gson.rawPathname);
     }
 
     @Test
@@ -46,5 +68,16 @@ public class MavenDependencyListParserTest {
         MavenRepositoryItem item = new MavenDependencyListParser().parseLine(line);
         System.out.format("parsed: %s%n", item);
         assertEquals("raw", "C:\\Users\\appveyor\\.m2\\repository\\com\\google\\code\\findbugs\\jsr305\\3.0.2\\jsr305-3.0.2.jar", item.rawPathname);
+    }
+
+    @Test
+    public void trimTrailingWhitespace() {
+        String s = "This is a string   \t\n";
+        String actual = MavenDependencyListParser.trimTrailingWhitespace(s);
+        assertEquals("trimmed", "This is a string", actual);
+
+        assertEquals("trimmed nothing", "", MavenDependencyListParser.trimTrailingWhitespace(""));
+        assertEquals("trimmed nothing", "NO_WHITESPACE", MavenDependencyListParser.trimTrailingWhitespace("NO_WHITESPACE"));
+        assertEquals("trimmed everything", "", MavenDependencyListParser.trimTrailingWhitespace("\t\n    \r\n   "));
     }
 }
